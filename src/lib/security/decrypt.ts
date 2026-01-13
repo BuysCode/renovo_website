d9cf187eac524e39707b63456ae8d91a8ce0598e
Guilherme Buys <131329633+BuysDev@users.noreply.github.com>
2026-01-13 17:31:11 -0300

    feat: creating RSA encrypt/decrypt system

---
+++ src/lib/security/decrypt.ts
@@ -0,0 +1,15 @@
+import crypto from 'crypto'
+
+export function decrypt(privateKey: string, encryptedData: string): string {
+  const buffer = Buffer.from(encryptedData, 'base64');
+  const decrypted = crypto.privateDecrypt(
+    {
+      key: privateKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: 'sha256'
+    },
+    buffer
+  );
+  
+  return decrypted.toString('utf8');
+}
